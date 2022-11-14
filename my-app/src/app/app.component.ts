854aa550faa6b85807279e87843828aeb815ac07
Hiral Dubal <hiral.dubal@1Rintern.local>
2022-11-14 17:18:56 +0530

    Image-upload

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app';
+
+  url = '../assets/images/Vector.png';
+  onSelect(event: any) {
+    let fileType = event.target.files[0].type;
+    if (fileType.match(/image\/*/)) {
+      let reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+    }
+    else
+    {
+      alert("Please select correct image format")
+    }
+  }
+}
